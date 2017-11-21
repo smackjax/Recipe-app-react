57eb847c57b8574fa86770871b1383e6c5483a82
smackjax <smackjax@gmail.com>
2017-11-21 18:51:20 -0500

    Create not connected component

---
+++ src/components/_not-connected/not-connected.component.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './not-connected.style.css';
+export default (props)=>{
+    return(
+        <div className="alert-danger not-connected-alert">
+            <i className="fa fa-wifi"></i>
+        </div>
+    )
+}
